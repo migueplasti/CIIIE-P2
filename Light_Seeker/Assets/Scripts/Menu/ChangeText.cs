3069513138550ce6ebeca46d126137eb98c12b93
sergio.mtrabajo <sergio.mtrabajo@udc.es>
2021-05-14 12:01:13 +0200

    modificado menu con objetivos y avance entre niveles

---
+++ Light_Seeker/Assets/Scripts/Menu/ChangeText.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text changeText;
+    //public GameObject changeTextTo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextChange();
+    }
+
+    public void TextChange(){
+        if(SceneManager.GetActiveScene().buildIndex == 1){
+            changeText.text = "Encuentra refugio";
+            //changeTextTo.GetComponent<Text>().text = "Encuentra refugio";
+        }else if(SceneManager.GetActiveScene().buildIndex == 2){
+            changeText.text = "Huye del reino";
+            //changeTextTo.GetComponent<Text>().text = "Huye del reino";
+        }
+    }
+
+}
